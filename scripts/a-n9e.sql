9ade05fda88ef1444df4bc87677ffbb4e97789c8
kongfei <kongfei605@gmail.com>
2023-03-30 19:12:03 +0800

    6.0.0-ga.2

--- scripts/a-n9e.sql
+++ scripts/a-n9e.sql
@@ -1,5 +1,6 @@
 set names utf8mb4;
 
+drop database if exists n9e_v6;
 create database n9e_v6;
 use n9e_v6;
 
@@ -386,7 +387,7 @@ insert into metric_view(name, cate, configs) values('Host View', 0, '{"filters":
 CREATE TABLE `recording_rule` (
     `id` bigint unsigned not null auto_increment,
     `group_id` bigint not null default '0' comment 'group_id',
-    `datasource_id` bigint not null default 0 comment 'datasource id',
+    `datasource_ids` varchar(255) not null default '' comment 'datasource ids',
     `cluster` varchar(128) not null,
     `name` varchar(255) not null comment 'new metric name',
     `note` varchar(255) not null comment 'rule note',
